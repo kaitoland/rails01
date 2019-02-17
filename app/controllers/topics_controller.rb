0052e5ed2a11fbbe25ab004d22b3922607369e4d
kaitoland <kaitoland1011@gmail.com>
2019-02-18 00:36:15 +0900

    pryの導入

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,6 +13,7 @@ class TopicsController < ApplicationController
   def show
       @newResponse = Response.new(:topic_id => params[:id])
       @responses = Response.where(topic_id: params[:id])
+      binding.pry
   end
 
   # GET /topics/new
